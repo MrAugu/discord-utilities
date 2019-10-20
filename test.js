bbe9c383dc645a4b81e61f254c3784d188947d95
MrAugu <mraugu@yahoo.com>
2019-10-20 11:10:18 +0300

    AwaitReply & Wait

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
 const Utils = require("./index.js");
- console.log(Utils.toProperCase("tesst omg lel"));
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", async () => {
+  console.log("Waiting...");
+  await Utils.wait(5000);
+  console.log("Waited 5 seconds.");
+});
+
+client.login("NjI0NjE3ODA0NjExNDUyOTI5.XawUEw.yLm8y2OJURUb09H_lvtMOvsg2Bw");
